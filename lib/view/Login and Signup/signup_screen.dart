766859422946c10d01bc3e00ff8f54701fdbf574
Nandakishors369 <nandakishors8213@gmail.com>
2022-12-10 10:51:28 +0530

    first_commit

---
+++ lib/view/Login and Signup/signup_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:lottie/lottie.dart';
+
+class SignupScreen extends StatefulWidget {
+  const SignupScreen({super.key});
+
+  @override
+  State<SignupScreen> createState() => _SignupScreenState();
+}
+
+class _SignupScreenState extends State<SignupScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Column(
+        children: [LottieBuilder.asset("assets/lottie/38435-register.json")],
+      )),
+    );
+  }
+}
